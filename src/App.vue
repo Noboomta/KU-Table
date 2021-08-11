13fca7ef7e05bfb0e9cdde4f5ff388f22bfd2619
JAmoMES <atipat.pa@ku.th>
2021-08-12 02:50:36 +0700

    fixed nav bug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
 	<div id="app">
-		<navbar></navbar>
+		<div>
+			<nav class="h-16 flex items-center bg-green-500 px-5 justify-between z-10 fixed w-full">
+				<div id="middle" class="h-16 flex items-center m-auto justify-center absolute">
+						<h1 class="font-bold text-gray-200 text-2xl">KU-Table</h1>
+				</div>
+				<div id="left">
+					<img src="./assets/ku-table.jpg" class="w-10 h-10" alt="KU-Logo" />
+				</div>
+				<div id="right" class="flex">
+					<router-link
+						class="m-3 text-gray-200 hover:text-white transition duration-300"
+						to="/schedule"
+						>Schedule</router-link
+					>
+					<div
+						class="m-3 text-gray-200 hover:text-white transition duration-300 cursor-pointer"
+						@click="checkLogin"
+					>
+						{{ buttonText }}
+					</div>
+				</div>
+			</nav>
+    <div class="h-16"></div>
+    </div>
 		<div class="flex-grow flex flex-col">
 			<router-view @login="authStatus = true" />
 		</div>
@@ -9,15 +32,43 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import KuFooter from './components/KuFooter.vue'
+import KuFooter from '@/components/KuFooter.vue'
 
 export default {
 	components: {
-		Navbar,
 		KuFooter
+	},
+	name: "navbar",
+  data() {
+		return {
+			authStatus: false
+		};
+	},
+	computed: {
+		buttonText() {
+			return this.authStatus ? "Logout" : "Login";
+		}
+	},
+	created() {
+		if (localStorage.getItem("accesstoken")) {
+			this.authStatus = true;
+		}
+	},
+	methods: {
+		checkLogin: function() {
+			if (this.authStatus) {
+				localStorage.removeItem("accesstoken");
+				localStorage.removeItem("stdId");
+				this.authStatus = false;
+				this.$router.push("/login");
+			} else {
+				if (this.$route.path != "/login") {
+					this.$router.push("/login");
+				}
+			}
+		}
 	}
-}
+};
 </script>
 
 <style>
@@ -25,5 +76,12 @@ export default {
 	display: flex;
 	flex-direction: column;
 	min-height: 100vh;
+}
+nav {
+	box-shadow: 0px 4px 4px 0px #00000040;
+}
+#middle{
+    left: 50%;
+    transform: translateX(-50%);
 }
 </style>
