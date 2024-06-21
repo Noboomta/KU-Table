d0fd412636131726f051e10b8d1f75f958bca2d2
Atipat Pankong <atipat.p@lmwn.com>
2024-06-22 04:33:13 +0700

    fix: public icon path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  },
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd())
+
+	return {
+		plugins: [vue()],
+		server: {
+			port: parseInt(env.VITE_PORT) || 3000,
+		},
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+			extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+		},
+	}
 })
