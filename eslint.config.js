cb4f6b7940f10938eb04a61685b7b4fa4f038c08
Atipat Pankong <atipat.p@lmwn.com>
2024-06-23 03:43:35 +0700

    import order

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,24 @@
 import globals from 'globals'
 import path from 'node:path'
+
 import { fileURLToPath } from 'node:url'
+
 import js from '@eslint/js'
+
 import { FlatCompat } from '@eslint/eslintrc'
 import { includeIgnoreFile } from '@eslint/compat'
+
 import pluginVue from 'eslint-plugin-vue'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 })
+
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 export default [
@@ -24,7 +31,6 @@ export default [
         ...globals.node,
       },
     },
-
     rules: {},
   },
 ]
